8820a35837e86e423ab19b7b444ab14f26b4bd23
karan9505 <karanskhedkar@gmail.com>
2023-07-19 19:51:31 +0530

    PYTHON

--- PYTHON/BASIC/5.py
+++ PYTHON/BASIC/5.py
@@ -22,3 +22,25 @@ for i in range(-len(List),0,1): #Accessing using negitive index
 	print('List',i,':',List[i])
 
 List[0]=100; #Mutable
+
+#--------------------------------------------------------------------------------#
+
+#Tuple
+Tuple=(1,2,3,4,5);
+
+print(Tuple.__sizeof__())
+print('Tuple : ',List)
+for i in range(0,len(Tuple)): #Accessing using index
+	print('Tuple',i,':',Tuple[i])
+
+for i in range(-len(Tuple),0,1): #Accessing using negitive index
+	print('Tuple',i,':',Tuple[i])
+
+#Tuple[0]=100; Imutable
+
+#---------------------------------------------------------------------------------#
+String="Mystring"
+
+print(String	)
+print(String.__sizeof__	())
+print(type(String))
